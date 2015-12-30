7f626159423ec467367d023cb13f947b057c444e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-12-30 23:55:38 +0100

    add activate-script installer

--- installers/activate-script.py
+++ installers/activate-script.py
@@ -18,14 +18,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import installlib
+import os
 
-installlib.register('activate-script', 'installers/activate-script.py')
-installlib.register('ninja', 'installers/ninja.py', version='1.6.0')
-installlib.register('meson', 'installers/meson.py', version='0.27.0')
-installlib.register('qemu', 'installers/qemu.py', version='2.4.91', co_version='2.5.0-rc1')
-installlib.register('binutils', 'installers/binutils.py', version='2.25.1', cross_dir='cross', target='arm-eabi')
-installlib.register('gcc', 'installers/gcc.py', version='5.2.0', cross_dir='cross', target='arm-eabi',
-  gmp_version='6.0.0a', mpfr_version='3.1.3', mpc_version='1.0.3')
-installlib.register('craftr', 'installers/craftr.py', version='master', min_version='0.20.0')
-installlib.register('crosstool-ng', 'installers/crosstool-ng.py', version='1.22.0')
+
+def main():
+  with open('.activate', 'w') as fp:
+    fp.write('export PATH="{0}/bin:$PATH"\n'.format(settings['prefix']))
+
+
+if __name__ == '__main__':
+  main()
